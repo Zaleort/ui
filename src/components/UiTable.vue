8c5e866f55b0fdc82d75ce6b2caee694d060d5b3
Alexandre Henrique <zaleortgames@outlook.com>
2020-08-27 14:41:10 +0200

    Permitir Cabeceras fijas, scroll horizontal y mostrar texto vacío

--- src/components/UiTable.vue
+++ src/components/UiTable.vue
@@ -1,41 +1,54 @@
 <template>
-  <table
-    :class="{
-      'ui-table': true,
-      [`ui-table--${size}`]: true,
-    }"
-  >
-    <tr>
-      <th
-        v-for="(h, index) of headings"
-        :key="index"
-        class="ui-table__heading"
-        :style="{
-          width: h.width,
-          textAlign: h.align,
-        }"
-      >
-        {{ h.name }}
-        <div v-if="h.sortable" class="ui-table__order">
-          <ui-icon
-            icon="chevronUp"
-            size="small"
-            @click="order(h, 'ascendent')"
-          />
-          <ui-icon
-            icon="chevronDown"
-            size="small"
-            @click="order(h, 'descendent')"
-          />
-        </div>
-      </th>
-    </tr>
-    <slot />
-  </table>
+  <div class="ui-table__wrapper">
+    <table
+      :class="{
+        'ui-table': true,
+        [`ui-table--${size}`]: true,
+        'ui-table--stripe': stripe,
+      }"
+    >
+      <thead>
+        <th
+          v-for="(h, index) of headings"
+          :key="index"
+          :class="{
+            'ui-table__heading': true,
+            [`ui-table__heading--${size}`]: true,
+            'is-fixed': fixedHead,
+          }"
+          :style="{
+            width: h.width + 'px',
+            textAlign: h.align,
+          }"
+        >
+          {{ h.name }}
+          <div v-if="h.sortable" class="ui-table__order">
+            <ui-icon
+              icon="chevronUp"
+              size="small"
+              @click="order(h, 'ascendent')"
+            />
+            <ui-icon
+              icon="chevronDown"
+              size="small"
+              @click="order(h, 'descendent')"
+            />
+          </div>
+        </th>
+      </thead>
+      <tbody class="ui-table__body">
+        <slot />
+      </tbody>
+    </table>
+
+    <span v-if="emptyText && (!items || items.length === 0)" class="ui-table__empty-text">
+      {{ emptyText }}
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, provide } from 'vue';
 import { UiTableHeading, UiTableSortMethod } from '@/interfaces/Table';
 
 export default defineComponent({
@@ -60,11 +73,32 @@ export default defineComponent({
       type: String,
       default: 'normal',
     },
+
+    stripe: {
+      type: Boolean,
+      default: false,
+    },
+
+    emptyText: {
+      type: String,
+      default: null,
+    },
+
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+
+    fixedHead: {
+      type: Boolean,
+      default: true,
+    },
   },
 
   emits: ['sort'],
 
   setup(props, context) {
+    provide('tableSize', props.size);
     const order = (item: UiTableHeading, direction: string) => {
       if (typeof props.sortMethod === 'function') {
         context.emit('sort', props.sortMethod(item.prop, direction));
