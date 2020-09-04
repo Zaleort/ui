7bb06d125074938eb25741bbbb7e7796c687e001
Alexandre Henrique <zaleortgames@outlook.com>
2020-09-04 10:25:55 +0200

    Fix: Warning formValidators is not defined

--- src/composables/formInject.ts
+++ src/composables/formInject.ts
@@ -2,8 +2,8 @@ import { inject } from 'vue';
 
 export default () => {
   const formDisabled = inject('formDisabled', false);
-  const formSize = inject('formSize', null);
-  const formValidators = inject('formValidators');
+  const formSize = inject<string | null>('formSize', null);
+  const formValidators = inject<Function | null>('formValidators', null);
   const formValidateOn = inject('formValidateOn', 'submit');
 
   return {
