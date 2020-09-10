e4904921f053adc45794858d2f6c594c5e73eb2f
Alexandre Henrique <zaleortgames@outlook.com>
2020-09-10 21:21:29 +0200

    Componente: Avatar

---
+++ src/components/UiAvatar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    :class="{
+      'ui-avatar': true,
+      'ui-avatar--circle': circle,
+      [`ui-avatar--${size}`]: true,
+    }"
+  >
+    <img :src="src" :alt="alt">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UiAvatar',
+  props: {
+    src: {
+      type: String,
+      default: null,
+    },
+
+    alt: {
+      type: String,
+      default: 'avatar',
+    },
+
+    circle: {
+      type: Boolean,
+      default: false,
+    },
+
+    size: {
+      type: String,
+      default: 'normal',
+    },
+  },
+});
+</script>
